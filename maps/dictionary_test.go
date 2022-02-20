d08e5946e0c73fa1ff1c1d5ce7480f2e1d2a3028
Abinav Bukkaraya <abinav@gmail.com>
2022-02-20 08:42:02 -0500

    add simple dictionary search in maps chapter

---
+++ maps/dictionary_test.go
@@ -0,0 +1,17 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	got := dictionary.Search("test")
+	want := "this is just a test"
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
