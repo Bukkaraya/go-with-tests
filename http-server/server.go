8cd4842bfdc58d3d9175b76f8bf4ecc4e794cb17
Abinav Bukkaraya <abinav.bukkarayasamudram@molex.com>
2022-04-06 15:23:37 -0400

    add endpoint for league data

--- http-server/server.go
+++ http-server/server.go
@@ -1,28 +1,61 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 )
 
+type Player struct {
+	Name string
+	Wins int
+}
+
 type PlayerStore interface {
 	GetPlayerScore(name string) int
 	RecordWin(name string)
+	GetLeague() []Player
 }
 
 type PlayerServer struct {
 	store PlayerStore
+	http.Handler
 }
 
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+const jsonContentType = "application/json"
 
-	switch r.Method {
-	case http.MethodPost:
-		p.processWin(w, player)
-	case http.MethodGet:
-		p.showScore(w, player)
+func NewPlayerServer(store PlayerStore) *PlayerServer {
+	p := new(PlayerServer)
+
+	p.store = store
+
+	router := http.NewServeMux()
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler()))
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler()))
+
+	p.Handler = router
+
+	return p
+}
+
+func (p *PlayerServer) playersHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+		switch r.Method {
+		case http.MethodPost:
+			p.processWin(w, player)
+		case http.MethodGet:
+			p.showScore(w, player)
+		}
+	}
+}
+
+func (p *PlayerServer) leagueHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", jsonContentType)
+		json.NewEncoder(w).Encode(p.store.GetLeague())
 	}
 }
 
